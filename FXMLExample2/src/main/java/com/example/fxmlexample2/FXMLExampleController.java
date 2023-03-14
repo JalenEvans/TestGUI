21f74b5757c24ad7c2163921b63b6ddc38527ac9
JalenEvans <jalenemmanuelevans@gmail.com>
2023-03-14 15:08:39 -0400

    Added a file chooser to select the pokemon you want to see

--- FXMLExample2/src/main/java/com/example/fxmlexample2/FXMLExampleController.java
+++ FXMLExample2/src/main/java/com/example/fxmlexample2/FXMLExampleController.java
@@ -6,12 +6,17 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FXMLExampleController {
+
+    private Stage stage;
     @FXML
     private TextField nameField;
 
@@ -25,6 +30,9 @@ public class FXMLExampleController {
     private Button showBtn;
 
     @FXML
+    private Button pickBtn;
+
+    @FXML
     private ImageView pokemon;
 
     @FXML
@@ -33,14 +41,35 @@ public class FXMLExampleController {
     private HashMap<String, String> pokeNames = new HashMap<>();
 
     public FXMLExampleController() {
-        pokeNames.put("bulbasaur", "src/main/resources/Pokemon_Images/bulbasaur.png");
+        pokeNames.put("bulbasaur", "C:\\Users\\redfl\\Desktop\\CIRDLES\\Pokemon\\bulbasaur.png");
+        pokeNames.put("charmander", "C:\\Users\\redfl\\Desktop\\CIRDLES\\Pokemon\\charmander.png");
+        pokeNames.put("squirtle", "C:\\Users\\redfl\\Desktop\\CIRDLES\\Pokemon\\squirtle.png");
+        pokeNames.put("pikachu", "C:\\Users\\redfl\\Desktop\\CIRDLES\\Pokemon\\pikachu.png");
+
+        stage = new Stage();
     }
 
     @FXML
     private void showPokemon() {
-        name = "C:\\Users\\redfl\\Desktop\\bulbasaur.png"; //nameField.getText();
-        pokeImage = new Image("C:\\Users\\redfl\\Desktop\\CIRDLES\\Pokemon\\bulbasaur.png");
+        name = nameField.getText();
+        pokeImage = new Image(pokeNames.get(name));
         pokemon.setImage(pokeImage);
-        testLabel.setText(pokeNames.get(name));
+        testLabel.setText(name);
     }
+    @FXML
+    private void pickPokemon() {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+
+        if (file != null) {
+            testLabel.setText(removeExtension(file.getName()));
+            pokeImage = new Image(file.getAbsolutePath());
+            pokemon.setImage(pokeImage);
+        }
+        else {
+            testLabel.setText("Nothing new was selected");
+        }
+    }
+
+    private String removeExtension (String filename) { return filename.split("\\.")[0]; }
 }
